f0eab9b890a22a6f8c4c9d66a711f3a074e40e4f
saiefsmairi <saieftaher@gmail.com>
2022-06-17 17:32:35 +0100

    jwt authentification

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var config = require("./database/db.json");
+var cors = require("cors");
+var mongoose = require("mongoose");
+const dotenv = require('dotenv').config();
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/userRoutes');
 
 var app = express();
 
@@ -20,11 +23,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', require('./routes/userRoutes'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+});
+
+
+mongoose.connect(config.mongo.uri, () => {
+  console.log("Connected to DATABASE");
 });
 
 // error handler
---
+++ models/user.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+
+var User = new Schema(
+    {
+        firstName: String,
+        lastName: String,
+        email: {
+            type: "string",
+            trim: true,
+            unique: true,
+        },
+        password: String,
+        phone: Number,
+        role: String,
+        verified: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        image: {
+            type: String,
+            default: "",
+        },
+        resetpassword: String,
+        accepted: {
+            type: Boolean,
+            default: false,
+        },
+
+    }
+);
+
+
+module.exports = mongoose.model("users", User);
