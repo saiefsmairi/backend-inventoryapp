64955398f89f5e9bb05146920902827cea3f38c6
saiefsmairi <saieftaher@gmail.com>
2022-07-11 15:32:30 +0100

    add , delete area from company done

---
+++ controllers/areaController.js
@@ -0,0 +1,111 @@
+const asyncHandler = require('express-async-handler')
+const Company = require('../models/company')
+const User = require('../models/user')
+const Area = require('../models/area')
+
+// @desc    add new area and assign it a company
+// @route   POST /area
+// @access  Public
+const addArea = asyncHandler(async (req, res) => {
+    const { code, namearea, companyid } = req.body
+    console.log(req.body)
+    const area = await Area.create(
+        {
+            code,
+            name: namearea,
+        }
+    )
+    const company = Company.findById({ _id: req.body.companyid })
+    Area.findById({ _id: area._id }, function (err, user) {
+        if (company && user) {
+            try {
+                Company.findByIdAndUpdate({ _id: req.body.companyid }, { $push: { areas: { area: user } } }, function (err, ff) {
+                    if (err) {
+                        res.status(400).json({
+                            status: 'failed',
+                            message: err,
+                        });
+                    }
+                    res.status(200).json({
+                        status: 'success',
+                        message: 'an area has been added to the company succesfully  ',
+                        data: {
+                            company: ff,
+                        },
+                    });
+                })
+
+            } catch (error) {
+                console.log(error)
+            }
+
+        } else {
+            res.status(400)
+            throw new Error('no company or user with that id')
+        }
+
+
+    })
+
+
+})
+
+
+const deleteAreaFromCompany = async (req, res, next) => {
+
+    Area.findById({ _id: req.body.areaid }, function (err, user) {
+
+        const company = Company.findById({ _id: req.body.companyid })
+        if (company) {
+            try {
+                Company.findByIdAndUpdate({ _id: req.body.companyid }, { $pull: { areas: { area: user } } }, function (err, ff) {
+                    console.log(ff)
+                    if (err) {
+                        res.status(400).json({
+                            status: 'failed',
+                            message: 'error is deleteAreaFromCompany method',
+                        });
+
+                    }
+                    else {
+                        Area.findByIdAndDelete(req.body.areaid);
+                        res.status(200).json({
+                            status: 'success',
+                            message: 'an area has been deleted from the company succesfully',
+                            data: {
+                                company: ff,
+                            },
+                        });
+                    }
+
+                })
+            } catch (error) {
+                console.log(error)
+            }
+
+        } else {
+            res.status(400)
+            throw new Error('no company with that id')
+        }
+
+    });
+
+
+    Area.findByIdAndDelete(req.body.areaid);
+
+};
+
+const deleteArea = async (req, res) => {
+    id = req.params.id;
+    console.log(id)
+    Area.findByIdAndDelete(id, (err, data) => {
+        res.status(200).json({ message: 'area deleteed' })
+
+    });
+}
+
+module.exports = {
+    addArea,
+    deleteAreaFromCompany,
+    deleteArea
+}
